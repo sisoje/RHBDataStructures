3a3744c8f833f946ad108a47c1da1035fd1f5ecf
Lazar Otasevic <lazar.otasevic@1und1.de>
2018-11-13 15:10:01 +0100

    using a class maybe better

--- Sources/Wrappers.swift
+++ Sources/Wrappers.swift
@@ -7,7 +7,7 @@ public class MutableWrapper<T> {
     }
 }
 
-public struct WeakObjectWrapper<T: AnyObject> {
+public class WeakObjectWrapper<T: AnyObject> {
     public private(set) weak var object: T?
     public init(_ object: T) {
         self.object = object
