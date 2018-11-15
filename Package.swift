c9e4f387e15f849a09c67b06c2e8f4b25c19c357
Lazar Otasevic <redhotbits@gmail.com>
2018-11-15 17:17:53 +0100

    fixed package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "RHBDataStructures",
+    name: "RHBFoundation",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "RHBDataStructures",
-            targets: ["RHBDataStructures"]),
+            name: "RHBFoundation",
+            targets: ["RHBFoundation"]),
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "RHBDataStructures",
+            name: "RHBFoundation",
             dependencies: [],
             path: "Sources"),
         ]
