e834638d9c8ee023f71401bd48a0633f9d5a96bd
Lazar Otasevic <redhotbits@gmail.com>
2018-11-26 23:59:30 +0100

    nscache wrapper

---
+++ Sources/GenericCache.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+public class GenericCache<H: Hashable, V: AnyObject> {
+    public let nsCache = NSCache<AnyObject, V>()
+    public init() {}
+}
+
+public extension GenericCache {
+    func set(key h: H, object v: V, cost: Int) {
+        nsCache.setObject(v, forKey: h as AnyObject, cost: cost)
+    }
+    subscript(_ h: H) -> V? {
+        get {
+            return nsCache.object(forKey: h as AnyObject)
+        }
+        set {
+            if let v = newValue {
+                nsCache.setObject(v, forKey: h as AnyObject)
+            } else {
+                nsCache.removeObject(forKey: h as AnyObject)
+            }
+        }
+    }
+}
