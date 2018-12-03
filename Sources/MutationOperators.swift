22b497b40f1e02a94e79843d7c3517276cb584a3
Lazar Otasevic <redhotbits@gmail.com>
2018-12-04 00:41:47 +0100

    add discardable

--- Sources/MutationOperators.swift
+++ Sources/MutationOperators.swift
@@ -3,11 +3,13 @@ import Foundation
 infix operator ~~: MultiplicationPrecedence
 infix operator ~: MultiplicationPrecedence
 
+@discardableResult
 public func ~<U: AnyObject>(object: U, block: (U) -> Void) -> U {
     block(object)
     return object
 }
 
+@discardableResult
 public func ~<U: AnyObject>(optional: U?, block: (U) -> Void) -> U? {
     if let object = optional {
         block(object)
