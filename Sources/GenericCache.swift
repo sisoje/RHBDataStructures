77499a7a534bfad117039aacdc606bb2df16b554
Lazar Otasevic <redhotbits@gmail.com>
2019-01-30 01:57:58 +0100

    cleanup

--- Sources/GenericCache.swift
+++ Sources/GenericCache.swift
@@ -6,9 +6,6 @@ open class GenericCache<H: Hashable, V: AnyObject> {
 }
 
 public extension GenericCache {
-    func set(key h: H, object v: V, cost: Int) {
-        nsCache.setObject(v, forKey: h as AnyObject, cost: cost)
-    }
     subscript(_ h: H) -> V? {
         get {
             return nsCache.object(forKey: h as AnyObject)
