1b898a7c009188e48b438bc05656357e197f714c
Lazar Otasevic <redhotbits@gmail.com>
2019-02-08 15:24:30 +0100

    fixed

--- Sources/NSPredicate+Typed.swift
+++ Sources/NSPredicate+Typed.swift
@@ -8,7 +8,7 @@ public class ComparisonPredicate<T>: NSComparisonPredicate, TypedPredicateProtoc
 
 public extension ComparisonPredicate {
     convenience init<S: Sequence>(inValues values: S) where S.Element == T {
-        let ex1 = NSExpression(forKeyPath: "self")
+        let ex1 = NSExpression(format: "self")
         let ex2 = NSExpression(forConstantValue: values)
         self.init(leftExpression: ex1, rightExpression: ex2, modifier: .direct, type: .in)
     }
