865a39d23420d2a0cdb2c10cf4706e9487f9798f
Lazar Otasevic <redhotbits@gmail.com>
2019-02-09 00:27:21 +0100

    removed unused file

--- Sources/NSSortDescriptor+Typed.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public class SortDescriptor<T>: NSSortDescriptor {}
-
-public extension KeyPath where Value: Comparable {
-    func sortDescriptor(ascending: Bool) -> SortDescriptor<Root> {
-        return SortDescriptor(keyPath: self, ascending: ascending)
-    }
-}
