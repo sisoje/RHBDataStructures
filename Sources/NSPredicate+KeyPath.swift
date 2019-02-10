a7ff5a8cb7d2f346a4607e1b20e5700b56996d1a
Lazar Otasevic <redhotbits@gmail.com>
2019-02-10 02:10:15 +0100

    reduced duplicate code

--- Sources/NSPredicate+KeyPath.swift
+++ Sources/NSPredicate+KeyPath.swift
@@ -1,15 +1,19 @@
 import Foundation
 
-public extension KeyPath {
-    func predicate(_ op: NSComparisonPredicate.Operator, _ value: Value) -> ComparisonPredicate<Root> {
+extension KeyPath {
+    func unconstraindedPredicate<V>(_ op: NSComparisonPredicate.Operator, _ value: V) -> ComparisonPredicate<Root> {
         let ex1 = NSExpression(forKeyPath: self)
         let ex2 = NSExpression(forConstantValue: value)
         return ComparisonPredicate(leftExpression: ex1, rightExpression: ex2, modifier: .direct, type: op)
     }
+}
+
+public extension KeyPath {
+    func predicate(_ op: NSComparisonPredicate.Operator, _ value: Value) -> ComparisonPredicate<Root> {
+        return unconstraindedPredicate(op, value)
+    }
     
     func predicate<T: Sequence>(_ op: NSComparisonPredicate.Operator, _ values: T) -> ComparisonPredicate<Root> where T.Element == Value {
-        let ex1 = NSExpression(forKeyPath: self)
-        let ex2 = NSExpression(forConstantValue: values)
-        return ComparisonPredicate(leftExpression: ex1, rightExpression: ex2, modifier: .direct, type: op)
+        return unconstraindedPredicate(op, values)
     }
 }
