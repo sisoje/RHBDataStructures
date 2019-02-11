99540f1fdcef3f047a42218d86f216179b89340a
Lazar Otasevic <redhotbits@gmail.com>
2019-02-11 02:15:45 +0100

    swift 5 ios 11

--- Sources/NSPredicate+Typed.swift
+++ Sources/NSPredicate+Typed.swift
@@ -3,11 +3,3 @@ import Foundation
 public protocol TypedPredicateProtocol { associatedtype Root }
 public class CompoundPredicate<T>: NSCompoundPredicate, TypedPredicateProtocol { public typealias Root = T }
 public class ComparisonPredicate<T>: NSComparisonPredicate, TypedPredicateProtocol { public typealias Root = T }
-
-public extension ComparisonPredicate {
-    convenience init<S: Sequence>(inValues values: S) where S.Element == T {
-        let ex1 = NSExpression(format: "self")
-        let ex2 = NSExpression(forConstantValue: values)
-        self.init(leftExpression: ex1, rightExpression: ex2, modifier: .direct, type: .in)
-    }
-}
