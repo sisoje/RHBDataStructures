6665d7ffcf988cdc7d2969266b0a04c5d7a76d2f
Lazar Otasevic <redhotbits@gmail.com>
2019-02-19 10:17:29 +0100

    added a noop

--- Sources/DeinitBlock.swift
+++ Sources/DeinitBlock.swift
@@ -10,6 +10,10 @@ public class DeinitBlock {
     }
 }
 
+public extension DeinitBlock {
+    func noop() {}
+}
+
 public extension Notification.Name {
     func addSmartObserver(center: NotificationCenter = .default, object: Any? = nil, queue: OperationQueue? = nil, _ block: @escaping (Notification) -> Void) -> DeinitBlock {
         return center.addSmartObserver(name: self, object: object, queue: queue, block)
