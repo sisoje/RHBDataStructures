00d66f31cdad1ffe0548d2d8b823e4a91bfc5f93
Lazar Otasevic <redhotbits@gmail.com>
2019-02-27 16:57:30 +0100

    renaming

--- Tests/DeinitBlock.swift
+++ Tests/DeinitBlock.swift
@@ -1,7 +1,26 @@
 import XCTest
 import RHBFoundation
 
-final class SmartObserverTests: XCTestCase {
+final class DeinitBlockTests: XCTestCase {
+    func testBlockDeinitBlock() {
+        var t = 0
+        _ = {
+            XCTAssert(t == 0)
+            _ = DeinitBlock { t = 1 }
+        }()
+        XCTAssert(t == 1)
+    }
+
+    func testBlockDeinitBlockArray() {
+        var t = 0
+        _ = {
+            XCTAssert(t == 0)
+            var m: [DeinitBlock] = []
+            m.append ( DeinitBlock { t = 1 } )
+        }()
+        XCTAssert(t == 1)
+    }
+
     func testNotificationCenter() {
         let notificationName = NSNotification.Name("testNotificationCenter")
         let exp = self.expectation(description: notificationName.rawValue)
