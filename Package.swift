6c8b22a85a34633bc0d57d6cdac09b39cd65bce2
Lazar Otasevic <redhotbits@gmail.com>
2019-02-27 16:33:19 +0100

    package with tests

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RHBFoundation",
     platforms: [
-        .macOS(.v10_13), .iOS(.v10_3)
+        .macOS(.v10_13), .iOS("10.3")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -28,6 +28,6 @@ let package = Package(
         .testTarget(
             name: "Tests",
             dependencies: ["RHBFoundation"],
-            psth: "Tests"),
+            path: "Tests"),
         ]
 )
