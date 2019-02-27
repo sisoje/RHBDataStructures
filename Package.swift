2a9a860481b406504a92fe744db58f9f03d5abb2
Lazar Otasevic <redhotbits@gmail.com>
2019-02-28 00:51:39 +0100

    decrease macos ver

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RHBFoundation",
     platforms: [
-        .macOS(.v10_13), .iOS("10.3")
+        .macOS(.v10_12), .iOS("10.3")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
