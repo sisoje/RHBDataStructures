86632ffcae1118476dd03dd0fd2b34445f034719
Lazar Otasevic <redhotbits@gmail.com>
2019-02-28 01:30:43 +0100

    simpler tests

--- Tests/CollectionExtensionsTests.swift
+++ Tests/CollectionExtensionsTests.swift
@@ -4,16 +4,15 @@ import RHBFoundation
 final class CollectionExtensionsTests: XCTestCase {
     func testEmpty() {
         let emptyArray = [Int]()
-        XCTAssertNil(emptyArray[safe: 0])
-        XCTAssertNil(emptyArray[safe: -1])
-        XCTAssertNil(emptyArray[safe: 1])
+        (-1...1).forEach {
+            XCTAssertNil(emptyArray[safe: $0])
+        }
     }
 
-    func testEdgeCases() {
+    func testValidValues() {
         let someArray = [1,2,3]
-        XCTAssertNil(someArray[safe: -1])
-        XCTAssertNil(someArray[safe: someArray.count])
-        XCTAssertNotNil(someArray[safe: 0])
-        XCTAssertNotNil(someArray[safe: someArray.count-1])
+        someArray.enumerated().forEach { index, val in
+            XCTAssert(someArray[safe: index] == val)
+        }
     }
 }
