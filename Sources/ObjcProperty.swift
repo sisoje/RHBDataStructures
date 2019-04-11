3f396c05bff32959eb022d527ef9163fa6a2830e
Lazar Otasevic <redhotbits@gmail.com>
2019-04-12 00:15:35 +0200

    prop list getter

---
+++ Sources/ObjcProperty.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public extension objc_property_t {
+    func typeInfo() -> String {
+        return NSString(utf8String: property_getAttributes(self)!)! as String
+    }
+    
+    func propertyName() -> String {
+        return NSString(utf8String: property_getName(self))! as String
+    }
+
+    static func propertyList<T: NSObject>(_ t: T.Type) -> [objc_property_t] {
+        var count32 = UInt32()
+        guard let classPropertyList = class_copyPropertyList(t, &count32) else {
+            return []
+        }
+        return (0..<Int(count32)).map { classPropertyList[$0] }
+    }
+}
