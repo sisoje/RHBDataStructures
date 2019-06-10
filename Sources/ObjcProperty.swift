b1554ef2de140a65a8749c1d97bfeed5e341fa6f
Lazar Otasevic <redhotbits@gmail.com>
2019-06-10 19:33:54 +0200

    fixed memory leak

--- Sources/ObjcProperty.swift
+++ Sources/ObjcProperty.swift
@@ -14,6 +14,9 @@ public extension objc_property_t {
         guard let classPropertyList = class_copyPropertyList(t, &count32) else {
             return []
         }
+        defer {
+            free(classPropertyList)
+        }
         return (0..<Int(count32)).map { classPropertyList[$0] }
     }
 }
